9a968054c6d82a248fdcc63b61f1d83f9ff27ff3
maimuzo <y.omichi@neoscorp.jp>
2019-06-26 17:45:47 +0900

    modified by formatter, and adjustment for mode-init code

--- dragino_lora_app_jp/main_jp.c
+++ dragino_lora_app_jp/main_jp.c
@@ -24,77 +24,76 @@
 #include <wiringPi.h>
 #include <wiringPiSPI.h>
 
-
 // #############################################
 // #############################################
 
-#define REG_FIFO                    0x00
-#define REG_OPMODE                  0x01
-#define REG_FIFO_ADDR_PTR           0x0D
-#define REG_FIFO_TX_BASE_AD         0x0E
-#define REG_FIFO_RX_BASE_AD         0x0F
-#define REG_RX_NB_BYTES             0x13
-#define REG_FIFO_RX_CURRENT_ADDR    0x10
-#define REG_IRQ_FLAGS               0x12
-#define REG_DIO_MAPPING_1           0x40
-#define REG_DIO_MAPPING_2           0x41
-#define REG_MODEM_CONFIG            0x1D
-#define REG_MODEM_CONFIG2           0x1E
-#define REG_MODEM_CONFIG3           0x26
-#define REG_SYMB_TIMEOUT_LSB  		0x1F
-#define REG_PKT_SNR_VALUE			0x19
-#define REG_PAYLOAD_LENGTH          0x22
-#define REG_IRQ_FLAGS_MASK          0x11
-#define REG_MAX_PAYLOAD_LENGTH 		0x23
-#define REG_HOP_PERIOD              0x24
-#define REG_SYNC_WORD				0x39
-#define REG_VERSION	  				0x42
+#define REG_FIFO 0x00
+#define REG_OPMODE 0x01
+#define REG_FIFO_ADDR_PTR 0x0D
+#define REG_FIFO_TX_BASE_AD 0x0E
+#define REG_FIFO_RX_BASE_AD 0x0F
+#define REG_RX_NB_BYTES 0x13
+#define REG_FIFO_RX_CURRENT_ADDR 0x10
+#define REG_IRQ_FLAGS 0x12
+#define REG_DIO_MAPPING_1 0x40
+#define REG_DIO_MAPPING_2 0x41
+#define REG_MODEM_CONFIG 0x1D
+#define REG_MODEM_CONFIG2 0x1E
+#define REG_MODEM_CONFIG3 0x26
+#define REG_SYMB_TIMEOUT_LSB 0x1F
+#define REG_PKT_SNR_VALUE 0x19
+#define REG_PAYLOAD_LENGTH 0x22
+#define REG_IRQ_FLAGS_MASK 0x11
+#define REG_MAX_PAYLOAD_LENGTH 0x23
+#define REG_HOP_PERIOD 0x24
+#define REG_SYNC_WORD 0x39
+#define REG_VERSION 0x42
 
-#define PAYLOAD_LENGTH              0x40
+#define PAYLOAD_LENGTH 0x40
 
 // LOW NOISE AMPLIFIER
-#define REG_LNA                     0x0C
-#define LNA_MAX_GAIN                0x23
-#define LNA_OFF_GAIN                0x00
-#define LNA_LOW_GAIN		    	0x20
+#define REG_LNA 0x0C
+#define LNA_MAX_GAIN 0x23
+#define LNA_OFF_GAIN 0x00
+#define LNA_LOW_GAIN 0x20
 
-#define RegDioMapping1                             0x40 // common
-#define RegDioMapping2                             0x41 // common
+#define RegDioMapping1 0x40 // common
+#define RegDioMapping2 0x41 // common
 
-#define RegPaConfig                                0x09 // common
-#define RegPaRamp                                  0x0A // common
-#define RegPaDac                                   0x5A // common
+#define RegPaConfig 0x09 // common
+#define RegPaRamp 0x0A   // common
+#define RegPaDac 0x5A    // common
 
-#define SX72_MC2_FSK                0x00
-#define SX72_MC2_SF7                0x70
-#define SX72_MC2_SF8                0x80
-#define SX72_MC2_SF9                0x90
-#define SX72_MC2_SF10               0xA0
-#define SX72_MC2_SF11               0xB0
-#define SX72_MC2_SF12               0xC0
+#define SX72_MC2_FSK 0x00
+#define SX72_MC2_SF7 0x70
+#define SX72_MC2_SF8 0x80
+#define SX72_MC2_SF9 0x90
+#define SX72_MC2_SF10 0xA0
+#define SX72_MC2_SF11 0xB0
+#define SX72_MC2_SF12 0xC0
 
-#define SX72_MC1_LOW_DATA_RATE_OPTIMIZE  0x01 // mandated for SF11 and SF12
+#define SX72_MC1_LOW_DATA_RATE_OPTIMIZE 0x01 // mandated for SF11 and SF12
 
 // sx1276 RegModemConfig1
-#define SX1276_MC1_BW_125                0x70
-#define SX1276_MC1_BW_250                0x80
-#define SX1276_MC1_BW_500                0x90
-#define SX1276_MC1_CR_4_5            0x02
-#define SX1276_MC1_CR_4_6            0x04
-#define SX1276_MC1_CR_4_7            0x06
-#define SX1276_MC1_CR_4_8            0x08
+#define SX1276_MC1_BW_125 0x70
+#define SX1276_MC1_BW_250 0x80
+#define SX1276_MC1_BW_500 0x90
+#define SX1276_MC1_CR_4_5 0x02
+#define SX1276_MC1_CR_4_6 0x04
+#define SX1276_MC1_CR_4_7 0x06
+#define SX1276_MC1_CR_4_8 0x08
 
-#define SX1276_MC1_IMPLICIT_HEADER_MODE_ON    0x01
+#define SX1276_MC1_IMPLICIT_HEADER_MODE_ON 0x01
 
 // sx1276 RegModemConfig2
-#define SX1276_MC2_RX_PAYLOAD_CRCON        0x04
+#define SX1276_MC2_RX_PAYLOAD_CRCON 0x04
 
 // sx1276 RegModemConfig3
-#define SX1276_MC3_LOW_DATA_RATE_OPTIMIZE  0x08
-#define SX1276_MC3_AGCAUTO                 0x04
+#define SX1276_MC3_LOW_DATA_RATE_OPTIMIZE 0x08
+#define SX1276_MC3_AGCAUTO 0x04
 
 // preamble for lora networks (nibbles swapped)
-#define LORA_MAC_PREAMBLE                  0x34
+#define LORA_MAC_PREAMBLE 0x34
 
 #define RXLORA_RXMODE_RSSI_REG_MODEM_CONFIG1 0x0A
 #ifdef LMIC_SX1276
@@ -104,26 +103,26 @@
 #endif
 
 // FRF
-#define        REG_FRF_MSB              0x06
-#define        REG_FRF_MID              0x07
-#define        REG_FRF_LSB              0x08
+#define REG_FRF_MSB 0x06
+#define REG_FRF_MID 0x07
+#define REG_FRF_LSB 0x08
 
-#define        FRF_MSB                  0xD9 // 868.1 Mhz
-#define        FRF_MID                  0x06
-#define        FRF_LSB                  0x66
+#define FRF_MSB 0xD9 // 868.1 Mhz
+#define FRF_MID 0x06
+#define FRF_LSB 0x66
 
 // ----------------------------------------
 // Constants for radio registers
-#define OPMODE_LORA      0x80
-#define OPMODE_MASK      0x07
-#define OPMODE_SLEEP     0x00
-#define OPMODE_STANDBY   0x01
-#define OPMODE_FSTX      0x02
-#define OPMODE_TX        0x03
-#define OPMODE_FSRX      0x04
-#define OPMODE_RX        0x05
+#define OPMODE_LORA 0x80
+#define OPMODE_MASK 0x07
+#define OPMODE_SLEEP 0x00
+#define OPMODE_STANDBY 0x01
+#define OPMODE_FSTX 0x02
+#define OPMODE_TX 0x03
+#define OPMODE_FSRX 0x04
+#define OPMODE_RX 0x05
 #define OPMODE_RX_SINGLE 0x06
-#define OPMODE_CAD       0x07
+#define OPMODE_CAD 0x07
 
 // ----------------------------------------
 // Bits masking the corresponding IRQs from the radio
@@ -137,11 +136,11 @@
 #define IRQ_LORA_CDDETD_MASK 0x01
 
 // DIO function mappings                D0D1D2D3
-#define MAP_DIO0_LORA_RXDONE   0x00  // 00------
-#define MAP_DIO0_LORA_TXDONE   0x40  // 01------
-#define MAP_DIO1_LORA_RXTOUT   0x00  // --00----
-#define MAP_DIO1_LORA_NOP      0x30  // --11----
-#define MAP_DIO2_LORA_NOP      0xC0  // ----11--
+#define MAP_DIO0_LORA_RXDONE 0x00 // 00------
+#define MAP_DIO0_LORA_TXDONE 0x40 // 01------
+#define MAP_DIO1_LORA_RXTOUT 0x00 // --00----
+#define MAP_DIO1_LORA_NOP 0x30    // --11----
+#define MAP_DIO2_LORA_NOP 0xC0    // ----11--
 
 // #############################################
 // #############################################
@@ -157,9 +156,17 @@ bool sx1272 = true;
 
 byte receivedbytes;
 
-enum sf_t { SF7=7, SF8, SF9, SF10, SF11, SF12 };
+enum sf_t
+{
+    SF7 = 7,
+    SF8,
+    SF9,
+    SF10,
+    SF11,
+    SF12
+};
 
-bool isReceiveFirstPacket = false;
+bool isReceivedFirstPacket = false;
 
 /*******************************************************************************
  *
@@ -169,8 +176,8 @@ bool isReceiveFirstPacket = false;
 
 // SX1272 - Raspberry connections
 int ssPin = 6;
-int dio0  = 7;
-int RST   = 0;
+int dio0 = 7;
+int RST = 0;
 
 // Set spreading factor (SF7 - SF12)
 sf_t sf = SF7;
@@ -222,21 +229,22 @@ void writeReg(byte addr, byte value)
     unselectreceiver();
 }
 
-static void opmode (uint8_t mode) {
+static void opmode(uint8_t mode)
+{
     writeReg(REG_OPMODE, (readReg(REG_OPMODE) & ~OPMODE_MASK) | mode);
 }
 
-static void opmodeLora() {
+static void opmodeLora()
+{
     uint8_t u = OPMODE_LORA;
     if (sx1272 == false)
-        u |= 0x8;   // TBD: sx1276 high freq
+        u |= 0x8; // TBD: sx1276 high freq
     writeReg(REG_OPMODE, u);
 }
 
-
 void SetupLoRa()
 {
-    
+
     digitalWrite(RST, HIGH);
     delay(100);
     digitalWrite(RST, LOW);
@@ -244,22 +252,28 @@ void SetupLoRa()
 
     byte version = readReg(REG_VERSION);
 
-    if (version == 0x22) {
+    if (version == 0x22)
+    {
         // sx1272
         printf("SX1272 detected, starting.\n");
         sx1272 = true;
-    } else {
+    }
+    else
+    {
         // sx1276?
         digitalWrite(RST, LOW);
         delay(100);
         digitalWrite(RST, HIGH);
         delay(100);
         version = readReg(REG_VERSION);
-        if (version == 0x12) {
+        if (version == 0x12)
+        {
             // sx1276
             printf("SX1276 detected, starting.\n");
             sx1272 = false;
-        } else {
+        }
+        else
+        {
             printf("Unrecognized transceiver.\n");
             //printf("Version: 0x%x\n",version);
             exit(1);
@@ -270,56 +284,70 @@ void SetupLoRa()
 
     // set frequency
     uint64_t frf = ((uint64_t)freq << 19) / 32000000;
-    writeReg(REG_FRF_MSB, (uint8_t)(frf>>16) );
-    writeReg(REG_FRF_MID, (uint8_t)(frf>> 8) );
-    writeReg(REG_FRF_LSB, (uint8_t)(frf>> 0) );
+    writeReg(REG_FRF_MSB, (uint8_t)(frf >> 16));
+    writeReg(REG_FRF_MID, (uint8_t)(frf >> 8));
+    writeReg(REG_FRF_LSB, (uint8_t)(frf >> 0));
 
     writeReg(REG_SYNC_WORD, 0x34); // LoRaWAN public sync word
 
-    if (sx1272) {
-        if (sf == SF11 || sf == SF12) {
-            writeReg(REG_MODEM_CONFIG,0x0B);
-        } else {
-            writeReg(REG_MODEM_CONFIG,0x0A);
+    if (sx1272)
+    {
+        if (sf == SF11 || sf == SF12)
+        {
+            writeReg(REG_MODEM_CONFIG, 0x0B);
         }
-        writeReg(REG_MODEM_CONFIG2,(sf<<4) | 0x04);
-    } else {
-        if (sf == SF11 || sf == SF12) {
-            writeReg(REG_MODEM_CONFIG3,0x0C);
-        } else {
-            writeReg(REG_MODEM_CONFIG3,0x04);
+        else
+        {
+            writeReg(REG_MODEM_CONFIG, 0x0A);
         }
-        writeReg(REG_MODEM_CONFIG,0x72);
-        writeReg(REG_MODEM_CONFIG2,(sf<<4) | 0x04);
+        writeReg(REG_MODEM_CONFIG2, (sf << 4) | 0x04);
+    }
+    else
+    {
+        if (sf == SF11 || sf == SF12)
+        {
+            writeReg(REG_MODEM_CONFIG3, 0x0C);
+        }
+        else
+        {
+            writeReg(REG_MODEM_CONFIG3, 0x04);
+        }
+        writeReg(REG_MODEM_CONFIG, 0x72);
+        writeReg(REG_MODEM_CONFIG2, (sf << 4) | 0x04);
     }
 
-    if (sf == SF10 || sf == SF11 || sf == SF12) {
-        writeReg(REG_SYMB_TIMEOUT_LSB,0x05);
-    } else {
-        writeReg(REG_SYMB_TIMEOUT_LSB,0x08);
+    if (sf == SF10 || sf == SF11 || sf == SF12)
+    {
+        writeReg(REG_SYMB_TIMEOUT_LSB, 0x05);
     }
-    writeReg(REG_MAX_PAYLOAD_LENGTH,0x80);
-    writeReg(REG_PAYLOAD_LENGTH,PAYLOAD_LENGTH);
-    writeReg(REG_HOP_PERIOD,0xFF);
+    else
+    {
+        writeReg(REG_SYMB_TIMEOUT_LSB, 0x08);
+    }
+    writeReg(REG_MAX_PAYLOAD_LENGTH, 0x80);
+    writeReg(REG_PAYLOAD_LENGTH, PAYLOAD_LENGTH);
+    writeReg(REG_HOP_PERIOD, 0xFF);
     writeReg(REG_FIFO_ADDR_PTR, readReg(REG_FIFO_RX_BASE_AD));
 
     writeReg(REG_LNA, LNA_MAX_GAIN);
-
 }
 
-boolean receive(char *payload) {
+boolean receive(char *payload)
+{
     // clear rxDone
     writeReg(REG_IRQ_FLAGS, 0x40);
 
     int irqflags = readReg(REG_IRQ_FLAGS);
 
     //  payload crc: 0x20
-    if((irqflags & 0x20) == 0x20)
+    if ((irqflags & 0x20) == 0x20)
     {
         printf("CRC error\n");
         writeReg(REG_IRQ_FLAGS, 0x20);
         return false;
-    } else {
+    }
+    else
+    {
 
         byte currentAddr = readReg(REG_FIFO_RX_CURRENT_ADDR);
         byte receivedCount = readReg(REG_RX_NB_BYTES);
@@ -327,7 +355,7 @@ boolean receive(char *payload) {
 
         writeReg(REG_FIFO_ADDR_PTR, currentAddr);
 
-        for(int i = 0; i < receivedCount; i++)
+        for (int i = 0; i < receivedCount; i++)
         {
             payload[i] = (char)readReg(REG_FIFO);
         }
@@ -335,85 +363,101 @@ boolean receive(char *payload) {
     return true;
 }
 
-void receivepacket() {
+void receivepacket()
+{
 
     long int SNR;
     int rssicorr;
 
-    if(digitalRead(dio0) == 1)
+    if (digitalRead(dio0) == 1)
     {
-        if(receive(message)) {
+        if (receive(message))
+        {
             byte value = readReg(REG_PKT_SNR_VALUE);
-            if( value & 0x80 ) // The SNR sign bit is 1
+            if (value & 0x80) // The SNR sign bit is 1
             {
                 // Invert and divide by 4
-                value = ( ( ~value + 1 ) & 0xFF ) >> 2;
+                value = ((~value + 1) & 0xFF) >> 2;
                 SNR = -value;
             }
             else
             {
                 // Divide by 4
-                SNR = ( value & 0xFF ) >> 2;
+                SNR = (value & 0xFF) >> 2;
             }
-            
-            if (sx1272) {
+
+            if (sx1272)
+            {
                 rssicorr = 139;
-            } else {
+            }
+            else
+            {
                 rssicorr = 157;
             }
 
-            printf("Packet RSSI: %d, ", readReg(0x1A)-rssicorr);
-            printf("RSSI: %d, ", readReg(0x1B)-rssicorr);
+            printf("Packet RSSI: %d, ", readReg(0x1A) - rssicorr);
+            printf("RSSI: %d, ", readReg(0x1B) - rssicorr);
             printf("SNR: %li, ", SNR);
             printf("Length: %i", (int)receivedbytes);
             printf("\n");
             printf("Payload: %s\n", message);
 
-            isReceiveFirstPacket = true;
+            isReceivedFirstPacket = true;
         } // received a message
 
     } // dio0=1
 }
 
-static void configPower (int8_t pw) {
-    if (sx1272 == false) {
+static void configPower(int8_t pw)
+{
+    if (sx1272 == false)
+    {
         // no boost used for now
-        if(pw >= 17) {
+        if (pw >= 17)
+        {
             pw = 15;
-        } else if(pw < 2) {
+        }
+        else if (pw < 2)
+        {
             pw = 2;
         }
         // check board type for BOOST pin
-        writeReg(RegPaConfig, (uint8_t)(0x80|(pw&0xf)));
-        writeReg(RegPaDac, readReg(RegPaDac)|0x4);
-
-    } else {
+        writeReg(RegPaConfig, (uint8_t)(0x80 | (pw & 0xf)));
+        writeReg(RegPaDac, readReg(RegPaDac) | 0x4);
+    }
+    else
+    {
         // set PA config (2-17 dBm using PA_BOOST)
-        if(pw > 17) {
+        if (pw > 17)
+        {
             pw = 17;
-        } else if(pw < 2) {
+        }
+        else if (pw < 2)
+        {
             pw = 2;
         }
-        writeReg(RegPaConfig, (uint8_t)(0x80|(pw-2)));
+        writeReg(RegPaConfig, (uint8_t)(0x80 | (pw - 2)));
     }
 }
 
-
-static void writeBuf(byte addr, byte *value, byte len) {                                                       
-    unsigned char spibuf[256];                                                                          
-    spibuf[0] = addr | 0x80;                                                                            
-    for (int i = 0; i < len; i++) {                                                                         
-        spibuf[i + 1] = value[i];                                                                       
-    }                                                                                                   
-    selectreceiver();                                                                                   
-    wiringPiSPIDataRW(CHANNEL, spibuf, len + 1);                                                        
-    unselectreceiver();                                                                                 
+static void writeBuf(byte addr, byte *value, byte len)
+{
+    unsigned char spibuf[256];
+    spibuf[0] = addr | 0x80;
+    for (int i = 0; i < len; i++)
+    {
+        spibuf[i + 1] = value[i];
+    }
+    selectreceiver();
+    wiringPiSPIDataRW(CHANNEL, spibuf, len + 1);
+    unselectreceiver();
 }
 
-void txlora(byte *frame, byte datalen) {
+void txlora(byte *frame, byte datalen)
+{
 
     // set the IRQ mapping DIO0=TxDone DIO1=NOP DIO2=NOP
-    writeReg(RegDioMapping1, MAP_DIO0_LORA_TXDONE|MAP_DIO1_LORA_NOP|MAP_DIO2_LORA_NOP);
+    writeReg(RegDioMapping1, MAP_DIO0_LORA_TXDONE | MAP_DIO1_LORA_NOP | MAP_DIO2_LORA_NOP);
     // clear all radio IRQ flags
     writeReg(REG_IRQ_FLAGS, 0xFF);
     // mask all IRQs but TxDone
@@ -432,12 +476,14 @@ void txlora(byte *frame, byte datalen) {
     printf("send: %s\n", frame);
 }
 
-long getEpochTime(){
-    
+long getEpochTimeSec()
+{
+
     return (unsigned long)time(NULL);
 }
 
-void sendMode(char *text){
+void sendMode(char *text)
+{
     opmodeLora();
     // enter standby mode (required for FIFO loading))
     opmode(OPMODE_STANDBY);
@@ -448,73 +494,87 @@ void sendMode(char *text){
     configPower(13); //maximum 13dBm in Japan
 
     printf("Enter send mode\n");
-    printf("Send packets at SF%i on %.6lf Mhz.\n", sf,(double)freq/1000000);
+    printf("Send packets at SF%i on %.6lf Mhz.\n", sf, (double)freq / 1000000);
     printf("------------------\n");
 
-    long startTime = getEpochTime();
-    for(int i=0; i<5; i++){
+    long startTime = getEpochTimeSec();
+    for (int i = 0; i < 5; i++)
+    {
         txlora(hello, strlen((char *)hello));
         delay(5000);
     }
-    long endTime = getEpochTime();
-    printf("start(sec): %lu, end(sec): %lu, delay(sec): %lu\n",  startTime, endTime, endTime - startTime);
+    long endTime = getEpochTimeSec();
+    printf("start(sec): %lu, end(sec): %lu, delay(sec): %lu\n", startTime, endTime, endTime - startTime);
 }
 
-void receiveMode(){
+void receiveMode()
+{
     // radio init
     opmodeLora();
     opmode(OPMODE_STANDBY);
     opmode(OPMODE_RX);
     printf("Enter receive mode\n");
-    printf("Listening at SF%i on %.6lf Mhz.\n", sf,(double)freq/1000000);
+    printf("Listening at SF%i on %.6lf Mhz.\n", sf, (double)freq / 1000000);
     printf("------------------\n");
-    
+
     // wait first packet
-    while(!isReceiveFirstPacket){
-        receivepacket(); 
+    while (!isReceivedFirstPacket)
+    {
+        receivepacket();
         delay(1);
     }
 
-    long startTime = getEpochTime();
+    long startTime = getEpochTimeSec();
     long endTime = startTime;
     long waitSec = 25;
 
     // wait delay
-    while(endTime < startTime + waitSec) {
-        receivepacket(); 
+    while (endTime < startTime + waitSec)
+    {
+        receivepacket();
         delay(1);
-        endTime = getEpochTime();
+        endTime = getEpochTimeSec();
     }
-    printf("start(sec): %lu, end(sec): %lu, delay(sec): %lu\n",  startTime, endTime, endTime - startTime);
+    printf("start(sec): %lu, end(sec): %lu, delay(sec): %lu\n", startTime, endTime, endTime - startTime);
 }
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
-    if (argc < 2) {
-        printf ("Usage: argv[0] sender|rec [message]\n");
+    if (argc < 2)
+    {
+        printf("Usage: argv[0] sender|rec [message]\n");
         exit(1);
     }
 
-    wiringPiSetup () ;
+    wiringPiSetup();
     pinMode(ssPin, OUTPUT);
     pinMode(dio0, INPUT);
     pinMode(RST, OUTPUT);
 
     wiringPiSPISetup(CHANNEL, 500000);
 
-    SetupLoRa();
-
-    if (!strcmp("sender", argv[1])) {
+    if (!strcmp("sender", argv[1]))
+    {
         if (argc > 2)
             strncpy((char *)hello, argv[2], sizeof(hello));
-        
+
+        SetupLoRa();
         sendMode((char *)hello);
+
+        SetupLoRa();
         receiveMode();
-    } else {
-        while(1){
+    }
+    else
+    {
+        while (1)
+        {
+            SetupLoRa();
             receiveMode();
+
+            SetupLoRa();
             sendMode(message);
-            isReceiveFirstPacket = false;
+            isReceivedFirstPacket = false;
         }
     }
 
